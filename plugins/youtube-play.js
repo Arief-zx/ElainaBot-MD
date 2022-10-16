41c32ce795900a20139bc10c8d9cdd8a688cd1cf
Arief-zx <77441608+Arief-zx@users.noreply.github.com>
2022-10-16 11:12:42 +0700

    Add files via upload

---
+++ plugins/youtube-play.js
@@ -0,0 +1,43 @@
+// Source? https://github/DikaArdnt/Hisoka-Morou
+// Recode By ImYanXiao
+import fetch from 'node-fetch'
+import yts from 'yt-search'
+let handler = async (m, { conn, groupMetadata, usedPrefix, text, args, command }) => {
+if (!text) throw `Example : ${usedPrefix + command} story wa anime`
+                let search = await yts(text)
+                let anu = search.videos[Math.floor(Math.random() * search.videos.length)]
+                let buttons = [
+                    {buttonId: `${usedPrefix} ytmp3 ${anu.url}`, buttonText: {displayText: '♫ Audio'}, type: 1},
+                    {buttonId: `${usedPrefix} ytmp4 ${anu.url}`, buttonText: {displayText: '► Video'}, type: 1}
+                ]
+                let buttonMessage = {
+                    image: { url: anu.thumbnail },
+                    caption: `
+⭔ Title : ${anu.title}
+⭔ Ext : Search
+⭔ ID : ${anu.videoId}
+⭔ Duration : ${anu.timestamp}
+⭔ Viewers : ${anu.views}
+⭔ Upload At : ${anu.ago}
+⭔ Author : ${anu.author.name}
+⭔ Channel : ${anu.author.url}
+⭔ Description : ${anu.description}
+⭔ Url : ${anu.url}`,
+                    footer: wm,
+                    buttons: buttons,
+                    headerType: 4
+                }
+                conn.sendMessage(m.chat, buttonMessage, { quoted: m })
+            }
+handler.help = ['play'].map(v => v + ' <pencarian>')
+handler.tags = ['downloader']
+handler.command = /^(y((outube|((utu|t)b|t))play|tp)|play(yt)?)$/i
+
+handler.exp = 0
+handler.limit = false
+
+export default handler
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
